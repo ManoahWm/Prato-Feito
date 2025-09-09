36f46eb8961d53e75e354abd4ee02a5febe7fecd
ManoahWm <manoahwalker14@gmail.com>
2025-09-09 15:29:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -25,3 +25,24 @@ function ler() {
 
   document.querySelector(".diminuir").style.display = "none";
 }
+//barra de pesquisa:
+
+function procurar(){
+    let pesquisa = document.getElementById("barraPesquisa").value;
+    console.log(pesquisa);
+    pesquisa = pesquisa.toLowerCase();
+    //console.log(pesquisa);
+    let elements = document.getElementsByClassName("card");
+    //console.log(elements);
+    //console.log(elements.length);
+    //console.log(elements[i].textContent.toLowerCase().includes(pesquisa));
+
+    for(i = 0; i < elements.length; i++){
+        if(!elements[i].textContent.toLowerCase().includes(pesquisa)){
+            elements[i].style.display = "none";
+        } else {
+            elements[i].style.display = "flex";
+        }
+    }
+
+}
